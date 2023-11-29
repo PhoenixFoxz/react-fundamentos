411c8a895c33598c5d43dd8adc1f06fec5e313cb
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-11-29 11:00:36 -0300

    Comentário dentro do JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 function App() {
-  return <>
+  // Comentário de uma linha CTRL ;
+  /* Comentário de várias linhas ALT SHIFT A */
+  return <> 
     <header>
       <h1>Olá React!</h1>
     </header>
+    {/* Comentário DENTRO do JSX 
+    É normal ficar entre {} nesta situação.*/}
     <main>
       <h2>Teste</h2>
     </main>
