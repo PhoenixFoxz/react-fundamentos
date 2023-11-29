9c9e09f8402aaeafeb172ae804f6c33f910960d5
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-11-29 11:50:10 -0300

    Separação de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,17 @@
+import Conteudo from "./components/Conteudo";
+import Header from "./components/Header";
+import Rodape from "./components/Rodape";
+
 function App() {
   // Comentário de uma linha CTRL ;
   /* Comentário de várias linhas ALT SHIFT A */
   return (
     <>
-      <header>
-        <h1>Olá React!</h1>
-        <hr />
-        <nav>
-          <a href="">Home</a>
-          <a href="">Produtos</a>
-          <a href="">Contato</a>
-        </nav>
-      </header>
+      <Header />
       {/* Comentário DENTRO do JSX 
     É normal ficar entre {} nesta situação.*/}
-      <main>
-        <section>
-          <h2>Conteúdo da aplicação</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos,
-            pariatur explicabo. Rerum excepturi corporis, esse quisquam ullam
-            cum et ad ea facere voluptas optio suscipit voluptatibus nulla
-            praesentium odio ut similique maxime recusandae molestias est labore
-            voluptatem non consequatur. Quam ratione, vero, itaque beatae maxime
-            amet facilis aperiam nostrum officiis dolore cupiditate, veritatis
-            ea dicta libero praesentium laboriosam quas voluptatibus culpa rem?
-            Minima odio cumque quaerat, corporis a eos quam nemo perspiciatis
-            sunt, incidunt consequuntur. Distinctio explicabo illo incidunt.
-            Officia laboriosam accusamus recusandae at iste. Cupiditate incidunt
-            ullam molestias voluptate et numquam excepturi porro sit ipsam quae.
-            Eveniet, officiis facilis?
-          </p>
-          <article>
-            <h3>Artigo 1</h3>
-            <p>Conteúdo do artigo 1...</p>
-          </article>
-          <article>
-            <h3>Artigo 2</h3>
-            <p>Conteúdo do artigo 2...</p>
-          </article>
-          <article>
-            <h3>Artigo 3</h3>
-            <p>Conteúdo do artigo 3...</p>
-          </article>
-        </section>
-      </main>
-      <footer>
-        <h2>Rodapé da aplicação</h2>
-      </footer>
+      <Conteudo />
+      <Rodape />
     </>
   );
 }
---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,37 @@
+function Conteudo() {
+  return (
+    <main>
+      <section>
+        <h2>Conteúdo da aplicação</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos,
+          pariatur explicabo. Rerum excepturi corporis, esse quisquam ullam cum
+          et ad ea facere voluptas optio suscipit voluptatibus nulla praesentium
+          odio ut similique maxime recusandae molestias est labore voluptatem
+          non consequatur. Quam ratione, vero, itaque beatae maxime amet facilis
+          aperiam nostrum officiis dolore cupiditate, veritatis ea dicta libero
+          praesentium laboriosam quas voluptatibus culpa rem? Minima odio cumque
+          quaerat, corporis a eos quam nemo perspiciatis sunt, incidunt
+          consequuntur. Distinctio explicabo illo incidunt. Officia laboriosam
+          accusamus recusandae at iste. Cupiditate incidunt ullam molestias
+          voluptate et numquam excepturi porro sit ipsam quae. Eveniet, officiis
+          facilis?
+        </p>
+        <article>
+          <h3>Artigo 1</h3>
+          <p>Conteúdo do artigo 1...</p>
+        </article>
+        <article>
+          <h3>Artigo 2</h3>
+          <p>Conteúdo do artigo 2...</p>
+        </article>
+        <article>
+          <h3>Artigo 3</h3>
+          <p>Conteúdo do artigo 3...</p>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default Conteudo;
