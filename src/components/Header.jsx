3a79ac7b1ff03f1d21ad52702b37a7cfbe548e76
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-11-30 10:47:40 -0300

    Conflito de classes no CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import Menu from "./Menu";
+import "./Header.css";
 
 function Header() {
   return (
     <header>
-      <h1>Olá React!</h1>
+      <h1>
+        Olá <span className="sombra">React!</span>
+      </h1>
       <hr />
       <Menu />
     </header>
