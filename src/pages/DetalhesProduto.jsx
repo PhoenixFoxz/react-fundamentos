714d8e0c9a01906e397aa127454df4338039a370
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-01-16 09:32:37 -0300

    Carregamento dos detalhes do produto

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function DetalhesProduto(){
+    /* Usamos o hook useParams do React Router DOM para ter acesso aos parâmetros da rotadinâmica neste caso, o parâmetro chamado "id". */
     const { id } = useParams();
+
+    useEffect( () => {
+        const carregarDados = async () => {            
+            try {
+                const resposta = await fetch(`https://fakestoreapi.com/products/${id}`)
+                const dados = await resposta.json();
+                console.log(dados);
+            } catch (error) {
+                console.error("Erro ao carregar produto: "+error)
+            }
+        }
+
+        carregarDados();
+    }, [id]);
 
     return <article>
         <h2>Título...</h2>
