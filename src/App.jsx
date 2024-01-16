b69295412da5b09e3624d8fbb82176b58f5df832
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-01-16 09:06:01 -0300

    DetalhesProduto

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Container from "./components/Container";
 import Produtos from "./pages/Produtos";
 import Contato from "./pages/Contato";
 import Rodape from "./components/Rodape";
+import DetalhesProduto from "./pages/DetalhesProduto";
 
 // importação de recursos/componentes do React Router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route Component={Home} exact path="/" />
             <Route Component={Produtos} path="/Produtos" />
+            <Route Component={DetalhesProduto} path="/produtos/:id" /> 
             <Route Component={Contato} path="/Contato" />
           </Routes>
         </Container>
